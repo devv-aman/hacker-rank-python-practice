c8216154044fe5993834e0eb93c52a67f8d921dc
entrepaman <entrepaman@gmail.com>
2018-03-01 21:44:49 +0530

    14 problems upto strings swap case

--- strings/p2.1-swap-case.py
+++ strings/p2.1-swap-case.py
@@ -1,1 +1,17 @@
 # Problem: https://www.hackerrank.com/challenges/swap-case/problem
+
+def swap_case(s):
+
+    swapped_string = ''
+
+    for x in s:
+        if x.islower():
+            swapped_string = swapped_string + x.upper()
+        else:
+            swapped_string = swapped_string + x.lower()
+    return swapped_string
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
